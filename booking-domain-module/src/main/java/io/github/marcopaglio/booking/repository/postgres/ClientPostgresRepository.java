89acce0ddf98a5c499bd377d51df74c10f98135d
marcopaglio <marco.paglio1997@gmail.com>
2023-08-10 22:18:37 +0200

    Added tests for find methods of ClientPostegresRepository

--- booking-domain-module/src/main/java/io/github/marcopaglio/booking/repository/postgres/ClientPostgresRepository.java
+++ booking-domain-module/src/main/java/io/github/marcopaglio/booking/repository/postgres/ClientPostgresRepository.java
@@ -9,12 +9,20 @@ import io.github.marcopaglio.booking.exception.UniquenessConstraintViolationExce
 import io.github.marcopaglio.booking.model.Client;
 import io.github.marcopaglio.booking.repository.ClientRepository;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
+
+import static io.github.marcopaglio.booking.model.Client.FIRSTNAME_DB;
+import static io.github.marcopaglio.booking.model.Client.LASTNAME_DB;
 
 /**
  * Implementation of repository layer through PostgreSQL for Client entities of the booking application.
  */
 public class ClientPostgresRepository implements ClientRepository {
 
+	/**
+	 * Entity Manager used to communicate with Hibernate provider.
+	 */
 	private EntityManager em;
 
 	public ClientPostgresRepository(EntityManager em) {
@@ -23,33 +31,48 @@ public class ClientPostgresRepository implements ClientRepository {
 
 	@Override
 	public List<Client> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery("SELECT c FROM Client c", Client.class).getResultList();
 	}
 
 	@Override
 	public Optional<Client> findById(UUID id) {
-		// TODO Auto-generated method stub
+		if (id != null) {
+			Client client = em.find(Client.class, id);
+			
+			if (client != null)
+				return Optional.of(client);
+		}
 		return Optional.empty();
 	}
 
 	@Override
 	public Optional<Client> findByName(String firstName, String lastName) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		TypedQuery<Client> query = em.createQuery(
+				"SELECT c FROM Client c WHERE c.firstName = :firstName AND c.lastName = :lastName",
+				Client.class);
+		query.setParameter("firstName", firstName);
+		query.setParameter("lastName", lastName);
+		
+		try {
+			Client client = query.getSingleResult();
+			return Optional.of(client);
+		} catch (NoResultException e) {
+			return Optional.empty();
+		}
 	}
 
 	@Override
 	public Client save(Client client) throws IllegalArgumentException, NotNullConstraintViolationException,
 			UniquenessConstraintViolationException {
-		// TODO Auto-generated method stub
+		if (client == null)
+			throw new IllegalArgumentException("Client to save cannot be null.");
 		return null;
 	}
 
 	@Override
 	public void delete(Client client) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-
+		if (client == null)
+			throw new IllegalArgumentException("Client to delete cannot be null.");
 	}
 
 }
--- booking-domain-module/src/test/java/io/github/marcopaglio/booking/repository/postgres/ClientPostgresRepositoryTest.java
+++ booking-domain-module/src/test/java/io/github/marcopaglio/booking/repository/postgres/ClientPostgresRepositoryTest.java
@@ -1,12 +1,17 @@
 package io.github.marcopaglio.booking.repository.postgres;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -22,13 +27,19 @@ import static io.github.marcopaglio.booking.model.Client.CLIENT_TABLE_DB;
 @DisplayName("Tests for ClientPostegresRepository class")
 @Testcontainers
 class ClientPostgresRepositoryTest {
+	private static final String A_FIRSTNAME = "Mario";
+	private static final String A_LASTNAME = "Rossi";
+	private static final UUID A_CLIENT_UUID = UUID.fromString("95021d62-9787-44e7-87ff-3edc17761b71");
 	
-	@Container
-	private static PostgreSQLContainer<?> postgreSQLContainer =
-		new PostgreSQLContainer<>("postgres:15.2")
-			.withDatabaseName("ClientPostgresRepositoryTest")
-			.withUsername("postgres-test")
-			.withPassword("postgres-test");
+	private static final String ANOTHER_FIRSTNAME = "Maria";
+	private static final String ANOTHER_LASTNAME = "De Lucia";
+	private static final UUID ANOTHER_CLIENT_UUID = UUID.fromString("2a942c02-642d-4cc1-b199-c11f6405bba2");
+
+	//@Container
+	private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15.2")
+		.withDatabaseName("ClientPostgresRepositoryTest_db")
+		.withUsername("postgres-test")
+		.withPassword("postgres-test");
 
 	private static EntityManagerFactory emf;
 	private EntityManager em;
@@ -37,6 +48,7 @@ class ClientPostgresRepositoryTest {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		postgreSQLContainer.start();
 		System.setProperty("db.port", postgreSQLContainer.getFirstMappedPort().toString());
 		System.setProperty("db.name", postgreSQLContainer.getDatabaseName());
 		
@@ -65,11 +77,145 @@ class ClientPostgresRepositoryTest {
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 		emf.close();
+		postgreSQLContainer.stop();
 	}
 
-	@Test
-	@DisplayName("ciao")
-	void test() {
-		assertThat(em.createQuery("SELECT c FROM Client c", Client.class).getResultList()).isEmpty();
+	@Nested
+	@DisplayName("Null inputs on methods")
+	class NullInputTest {
+
+		@Test
+		@DisplayName("Null id on 'findById'")
+		void testFindByIdWhenIdIsNullShouldReturnOptionalOfEmpty() {
+			assertThat(clientRepository.findById(null)).isEmpty();
+		}
+
+		@Test
+		@DisplayName("Null names on 'findByName'")
+		void testFindByNameWhenNamesAreNullShouldReturnOptionalOfEmpty() {
+			assertThat(clientRepository.findByName(null, A_LASTNAME)).isEmpty();
+			
+			assertThat(clientRepository.findByName(A_FIRSTNAME, null)).isEmpty();
+			
+			assertThat(clientRepository.findByName(null, null)).isEmpty();
+		}
+
+		@Test
+		@DisplayName("Null client on 'save'")
+		void testSaveWhenClientIsNullShouldThrow() {
+			assertThatThrownBy(() -> clientRepository.save(null))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("Client to save cannot be null.");
+		}
+
+		@Test
+		@DisplayName("Null client on 'delete'")
+		void testDeleteWhenClientIsNullShouldThrow() {
+			assertThatThrownBy(() -> clientRepository.delete(null))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("Client to delete cannot be null.");
+		}
+	}
+
+	@Nested
+	@DisplayName("Tests that read the database")
+	class ReadDBTest {
+		private Client client;
+		private Client another_client;
+
+		@BeforeEach
+		void resetClientId() throws Exception {
+			client = new Client(A_FIRSTNAME, A_LASTNAME);
+			another_client = new Client(ANOTHER_FIRSTNAME, ANOTHER_LASTNAME);
+		}
+
+		@Nested
+		@DisplayName("Tests for 'findAll'")
+		class FindAllTest {
+
+			@Test
+			@DisplayName("Database is empty")
+			void testFindAllWhenDatabaseIsEmptyShouldReturnEmptyList() {
+				assertThat(clientRepository.findAll()).isEmpty();
+			}
+
+			@Test
+			@DisplayName("Database is not empty")
+			void testFindAllWhenDatabaseIsNotEmptyShouldReturnClientsAsList() {
+				addTestClientToDatabase(client);
+				addTestClientToDatabase(another_client);
+				
+				assertThat(clientRepository.findAll())
+					.containsExactlyInAnyOrder(client, another_client);
+			}
+		}
+
+		@Nested
+		@DisplayName("Tests for 'findById'")
+		class FindByIdTest {
+
+			@Test
+			@DisplayName("Client is not in database")
+			void testFindByIdWhenClientIsNotInDatabaseShouldReturnOptionalOfEmpty() {
+				assertThat(clientRepository.findById(A_CLIENT_UUID)).isEmpty();
+			}
+
+			@Test
+			@DisplayName("Client is in database")
+			void testFindByIdWhenClientIsInDatabaseShouldReturnOptionalOfClient() {
+				addTestClientToDatabase(client);
+				addTestClientToDatabase(another_client);
+				
+				assertThat(clientRepository.findById(another_client.getId()))
+					.isEqualTo(Optional.of(another_client));
+			}
+			
+			//TODO: What if l'oggetto memorizzato viene cambiato? anche per mongo
+		}
+
+		@Nested
+		@DisplayName("Tests for 'findByName'")
+		class FindByNameTest {
+
+			@Test
+			@DisplayName("Client is not in database")
+			void testFindByNameWhenClientIsNotInDatabaseShouldReturnOptionalOfEmpty() {
+				assertThat(clientRepository.findByName(ANOTHER_FIRSTNAME, ANOTHER_LASTNAME)).isEmpty();
+			}
+
+			@Test
+			@DisplayName("Another same name client in database")
+			void testFindByNameWhenThereIsAnotherClientWithTheSameNameShouldNotMatchIt() {
+				addTestClientToDatabase(client);
+				
+				assertThat(clientRepository.findByName(A_FIRSTNAME, ANOTHER_LASTNAME))
+					.isNotEqualTo(Optional.of(client));
+			}
+
+			@Test
+			@DisplayName("Another same surname client in database")
+			void testFindByNameWhenThereIsAnotherClientWithTheSameSurnameShouldNotMatchIt() {
+				addTestClientToDatabase(client);
+				
+				assertThat(clientRepository.findByName(ANOTHER_FIRSTNAME, A_LASTNAME))
+					.isNotEqualTo(Optional.of(client));
+			}
+
+			@Test
+			@DisplayName("Client is in database")
+			void testFindByNameWhenClientIsInDatabaseShouldReturnOptionalOfClient() {
+				addTestClientToDatabase(client);
+				addTestClientToDatabase(another_client);
+				
+				assertThat(clientRepository.findByName(A_FIRSTNAME, A_LASTNAME))
+					.isEqualTo(Optional.of(client));
+			}
+		}
+		
+		private void addTestClientToDatabase(Client client) {
+			em.getTransaction().begin();
+			em.persist(client);
+			em.getTransaction().commit();
+		}
 	}
 }
