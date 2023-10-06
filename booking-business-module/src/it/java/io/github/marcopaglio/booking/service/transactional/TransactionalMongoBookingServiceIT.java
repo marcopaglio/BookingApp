7c08895cc353392c750da7e7aa2a70230fea37e0
marcopaglio <marco.paglio1997@gmail.com>
2023-10-06 18:29:24 +0200

    Configured databases in docker-maven-plugin

---
+++ booking-business-module/src/it/java/io/github/marcopaglio/booking/service/transactional/TransactionalMongoBookingServiceIT.java
@@ -0,0 +1,201 @@
+package io.github.marcopaglio.booking.service.transactional;
+
+import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
+import static io.github.marcopaglio.booking.repository.mongo.MongoRepository.BOOKING_DB_NAME;
+import static io.github.marcopaglio.booking.model.Client.CLIENT_TABLE_DB;
+import static io.github.marcopaglio.booking.model.Reservation.RESERVATION_TABLE_DB;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.bson.UuidRepresentation.STANDARD;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import static org.bson.codecs.pojo.Conventions.ANNOTATION_CONVENTION;
+import static org.bson.codecs.pojo.Conventions.USE_GETTERS_FOR_SETTERS;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.UUID;
+
+import org.bson.codecs.configuration.CodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import io.github.marcopaglio.booking.model.Client;
+import io.github.marcopaglio.booking.model.Reservation;
+import io.github.marcopaglio.booking.repository.factory.ClientRepositoryFactory;
+import io.github.marcopaglio.booking.repository.factory.ReservationRepositoryFactory;
+import io.github.marcopaglio.booking.transaction.handler.factory.TransactionHandlerFactory;
+import io.github.marcopaglio.booking.transaction.manager.mongo.TransactionMongoManager;
+
+@DisplayName("Integration tests of TransactionalBookingService and MongoDB")
+class TransactionalMongoBookingServiceIT {
+	private static final String A_LASTNAME = "Rossi";
+	private static final String A_FIRSTNAME = "Mario";
+	private static final String ANOTHER_LASTNAME = "De Lucia";
+	private static final String ANOTHER_FIRSTNAME = "Maria";
+
+	private static final UUID A_CLIENT_UUID = UUID.fromString("3ae9a113-9adc-41f8-b034-f5ddc0006d50");
+	private static final LocalDate A_LOCALDATE = LocalDate.parse("2023-04-24");
+	private static final UUID A_RESERVATION_UUID = UUID.fromString("232d9575-4ef7-461d-b489-dfdfe7a6c315");
+	private static final UUID ANOTHER_CLIENT_UUID = UUID.fromString("9ad2c2c0-9b55-4fe3-8404-74ab2869fc61");
+	private static final LocalDate ANOTHER_LOCALDATE = LocalDate.parse("2023-09-05");
+	private static final UUID ANOTHER_RESERVATION_UUID = UUID.fromString("5b8250e6-478f-4001-8870-09aae2995752");
+
+	private Client client, another_client;
+	private Reservation reservation, another_reservation;
+
+	private static MongoClient mongoClient;
+	private static MongoDatabase database;
+	private static MongoCollection<Client> clientCollection;
+	private static MongoCollection<Reservation> reservationCollection;
+
+	private static TransactionHandlerFactory transactionHandlerFactory;
+	private static ClientRepositoryFactory clientRepositoryFactory;
+	private static ReservationRepositoryFactory reservationRepositoryFactory;
+	private static TransactionMongoManager transactionManager;
+
+	private TransactionalBookingService service;
+
+	@BeforeAll
+	static void setupCollaborators() throws Exception {
+		mongoClient = getClient(System.getProperty("mongo.connectionString", "mongodb://localhost:27017"));
+		database = mongoClient.getDatabase(BOOKING_DB_NAME);
+		clientCollection = database.getCollection(CLIENT_TABLE_DB, Client.class);
+		reservationCollection = database.getCollection(RESERVATION_TABLE_DB, Reservation.class);
+		
+		transactionHandlerFactory = new TransactionHandlerFactory();
+		clientRepositoryFactory = new ClientRepositoryFactory();
+		reservationRepositoryFactory = new ReservationRepositoryFactory();
+		transactionManager = new TransactionMongoManager(mongoClient, transactionHandlerFactory,
+				clientRepositoryFactory, reservationRepositoryFactory);
+	}
+
+	private static MongoClient getClient(String connectionString) {
+		// define the CodecProvider for POJO classes
+		CodecProvider pojoCodecProvider = PojoCodecProvider.builder()
+				.conventions(Arrays.asList(ANNOTATION_CONVENTION, USE_GETTERS_FOR_SETTERS))
+				.automatic(true)
+				.build();
+		
+		// define the CodecRegistry as codecs and other related information
+		CodecRegistry pojoCodecRegistry =
+				fromRegistries(getDefaultCodecRegistry(),
+				fromProviders(pojoCodecProvider));
+		
+		// configure the MongoClient for using the CodecRegistry
+		MongoClientSettings settings = MongoClientSettings.builder()
+				.applyConnectionString(new ConnectionString(connectionString))
+				.uuidRepresentation(STANDARD)
+				.codecRegistry(pojoCodecRegistry)
+				.build();
+		return MongoClients.create(settings);
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		database.drop();
+		
+		service = new TransactionalBookingService(transactionManager);
+	}
+
+	@AfterAll
+	static void closeClient() throws Exception {
+		mongoClient.close();
+	}
+
+	@Nested
+	@DisplayName("Methods using only ClientMongoRepository")
+	class ClientMongoRepositoryTest {
+
+		@Nested
+		@DisplayName("Tests for 'findAllClients'")
+		class FindAllClientsTest {
+
+			@Test
+			@DisplayName("No clients to retrieve")
+			void testFindAllClientsWhenThereAreNoClientsToRetrieveShouldReturnEmptyList() {
+				assertThat(service.findAllClients()).isEmpty();
+			}
+
+			@Test
+			@DisplayName("Several clients to retrieve")
+			void testFindAllClientsWhenThereAreSeveralClientsToRetrieveShouldReturnClientsAsList() {
+				populateClientDatabase();
+				
+				assertThat(service.findAllClients()).isEqualTo(Arrays.asList(client, another_client));
+			}
+		}
+
+		private void populateClientDatabase() {
+			initClients();
+			saveClients();
+		}
+
+		private void initClients() {
+			client = new Client(A_FIRSTNAME, A_LASTNAME);
+			another_client = new Client(ANOTHER_FIRSTNAME, ANOTHER_LASTNAME);
+		}
+	
+		public void saveClients() {
+			client.setId(A_CLIENT_UUID);
+			clientCollection.insertOne(client);
+			another_client.setId(ANOTHER_CLIENT_UUID);
+			clientCollection.insertOne(another_client);
+		}
+	}
+
+	@Nested
+	@DisplayName("Methods using only ReservationMongoRepository")
+	class ReservationMongoRepositoryTest {
+
+		@Nested
+		@DisplayName("Tests for 'findAllReservations'")
+		class FindAllReservationsTest {
+
+			@Test
+			@DisplayName("No reservations to retrieve")
+			void testFindAllReservationsWhenThereAreNoReservationsToRetrieveShouldReturnEmptyList() {
+				assertThat(service.findAllReservations()).isEmpty();
+			}
+
+			@Test
+			@DisplayName("Several reservations to retrieve")
+			void testFindAllReservationsWhenThereAreSeveralReservationsToRetrieveShouldReturnReservationAsList() {
+				populateReservationDatabase();
+				
+				assertThat(service.findAllReservations())
+					.isEqualTo(Arrays.asList(reservation, another_reservation));
+			}
+		}
+
+		private void populateReservationDatabase() {
+			initReservations();
+			saveReservations();
+		}
+
+		private void initReservations() {
+			reservation = new Reservation(A_CLIENT_UUID, A_LOCALDATE);
+			another_reservation = new Reservation(ANOTHER_CLIENT_UUID, ANOTHER_LOCALDATE);
+		}
+	
+		public void saveReservations() {
+			reservation.setId(A_RESERVATION_UUID);
+			reservationCollection.insertOne(reservation);
+			another_reservation.setId(ANOTHER_RESERVATION_UUID);
+			reservationCollection.insertOne(another_reservation);
+		}
+	}
+
+}
---
+++ booking-business-module/src/it/java/io/github/marcopaglio/booking/service/transactional/TransactionalPostgresBookingServiceIT.java
@@ -0,0 +1,188 @@
+package io.github.marcopaglio.booking.service.transactional;
+
+import static io.github.marcopaglio.booking.model.Client.CLIENT_TABLE_DB;
+import static io.github.marcopaglio.booking.model.Reservation.RESERVATION_TABLE_DB;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.UUID;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import io.github.marcopaglio.booking.model.Client;
+import io.github.marcopaglio.booking.model.Reservation;
+import io.github.marcopaglio.booking.repository.factory.ClientRepositoryFactory;
+import io.github.marcopaglio.booking.repository.factory.ReservationRepositoryFactory;
+import io.github.marcopaglio.booking.transaction.handler.factory.TransactionHandlerFactory;
+import io.github.marcopaglio.booking.transaction.manager.postgres.TransactionPostgresManager;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+@DisplayName("Integration tests of TransactionalBookingService and PostgreSQL")
+class TransactionalPostgresBookingServiceIT {
+	private static final String A_LASTNAME = "Rossi";
+	private static final String A_FIRSTNAME = "Mario";
+	private static final String ANOTHER_LASTNAME = "De Lucia";
+	private static final String ANOTHER_FIRSTNAME = "Maria";
+
+	private static final UUID A_CLIENT_UUID = UUID.fromString("bc49bffa-0766-4e5d-90af-d8a6ef516df4");
+	private static final LocalDate A_LOCALDATE = LocalDate.parse("2023-04-24");
+	private static final UUID ANOTHER_CLIENT_UUID = UUID.fromString("c01a64c2-73e6-4b70-808f-00f9bd82571d");
+	private static final LocalDate ANOTHER_LOCALDATE = LocalDate.parse("2023-09-05");
+
+	private Client client, another_client;
+	private Reservation reservation, another_reservation;
+
+	private static EntityManagerFactory emf;
+	private static TransactionHandlerFactory transactionHandlerFactory;
+	private static ClientRepositoryFactory clientRepositoryFactory;
+	private static ReservationRepositoryFactory reservationRepositoryFactory;
+	private static TransactionPostgresManager transactionManager;
+
+	private TransactionalBookingService service;
+
+	@BeforeAll
+	static void setupCollaborators() throws Exception {
+		System.setProperty("db.port", System.getProperty("postgres.port", "5432"));
+		System.setProperty("db.name", System.getProperty("postgres.name", "IntegrationTest_db"));
+		emf = Persistence.createEntityManagerFactory("postgres-it");
+		
+		transactionHandlerFactory = new TransactionHandlerFactory();
+		clientRepositoryFactory = new ClientRepositoryFactory();
+		reservationRepositoryFactory = new ReservationRepositoryFactory();
+		transactionManager = new TransactionPostgresManager(emf, transactionHandlerFactory,
+				clientRepositoryFactory, reservationRepositoryFactory);
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		service = new TransactionalBookingService(transactionManager);
+	}
+
+	@AfterAll
+	static void closeEmf() throws Exception {
+		emf.close();
+	}
+
+	@Nested
+	@DisplayName("Methods using only ClientPostgresRepository")
+	class ClientPostgresRepositoryTest {
+
+		@BeforeEach
+		void setupDatabase() throws Exception {
+			EntityManager em = emf.createEntityManager();
+			
+			// make sure we always start with a clean database
+			em.getTransaction().begin();
+			em.createNativeQuery("TRUNCATE TABLE " + CLIENT_TABLE_DB).executeUpdate();
+			em.getTransaction().commit();
+			
+			em.close();
+		}
+
+		@Nested
+		@DisplayName("Tests for 'findAllClients'")
+		class FindAllClientsTest {
+
+			@Test
+			@DisplayName("No clients to retrieve")
+			void testFindAllClientsWhenThereAreNoClientsToRetrieveShouldReturnEmptyList() {
+				assertThat(service.findAllClients()).isEmpty();
+			}
+
+			@Test
+			@DisplayName("Several clients to retrieve")
+			void testFindAllClientsWhenThereAreSeveralClientsToRetrieveShouldReturnClientsAsList() {
+				populateClientDatabase();
+				
+				assertThat(service.findAllClients())
+					.isEqualTo(Arrays.asList(client, another_client));
+			}
+		}
+
+		private void populateClientDatabase() {
+			initClients();
+			saveClients();
+		}
+
+		private void initClients() {
+			client = new Client(A_FIRSTNAME, A_LASTNAME);
+			another_client = new Client(ANOTHER_FIRSTNAME, ANOTHER_LASTNAME);
+		}
+
+		private void saveClients() {
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			em.persist(client);
+			em.persist(another_client);
+			em.getTransaction().commit();
+			em.close();
+		}
+	}
+
+	@Nested
+	@DisplayName("Methods using only ReservationPostgresRepository")
+	class ReservationPostgresRepositoryTest {
+
+		@BeforeEach
+		void setupDatabase() throws Exception {
+			EntityManager em = emf.createEntityManager();
+			
+			// make sure we always start with a clean database
+			em.getTransaction().begin();
+			em.createNativeQuery("TRUNCATE TABLE " + RESERVATION_TABLE_DB).executeUpdate();
+			em.getTransaction().commit();
+			
+			em.close();
+		}
+
+		@Nested
+		@DisplayName("Tests for 'findAllReservations'")
+		class FindAllReservationsTest {
+
+			@Test
+			@DisplayName("No reservations to retrieve")
+			void testFindAllReservationsWhenThereAreNoReservationsToRetrieveShouldReturnEmptyList() {
+				System.out.println(UUID.randomUUID());
+				System.out.println(UUID.randomUUID());
+				
+				assertThat(service.findAllReservations()).isEmpty();
+			}
+
+			@Test
+			@DisplayName("Several reservations to retrieve")
+			void testFindAllReservationsWhenThereAreSeveralReservationsToRetrieveShouldReturnReservationAsList() {
+				populateReservationDatabase();
+				
+				assertThat(service.findAllReservations())
+					.isEqualTo(Arrays.asList(reservation, another_reservation));
+			}
+		}
+
+		private void populateReservationDatabase() {
+			initReservations();
+			saveReservations();
+		}
+
+		private void initReservations() {
+			reservation = new Reservation(A_CLIENT_UUID, A_LOCALDATE);
+			another_reservation = new Reservation(ANOTHER_CLIENT_UUID, ANOTHER_LOCALDATE);
+		}
+
+		private void saveReservations() {
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			em.persist(reservation);
+			em.persist(another_reservation);
+			em.getTransaction().commit();
+			em.close();
+		}
+	}
+}
