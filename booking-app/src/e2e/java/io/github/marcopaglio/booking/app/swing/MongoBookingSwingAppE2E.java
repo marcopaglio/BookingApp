e9fe5d27df0c5757cf49d741ae5fc3a91af57eb9
marcopaglio <marco.paglio1997@gmail.com>
2023-10-29 16:07:34 +0100

    E2E tests for use case 'add client'

--- booking-app/src/e2e/java/io/github/marcopaglio/booking/app/swing/MongoBookingSwingAppE2E.java
+++ booking-app/src/e2e/java/io/github/marcopaglio/booking/app/swing/MongoBookingSwingAppE2E.java
@@ -14,6 +14,8 @@ import static org.bson.codecs.pojo.Conventions.ANNOTATION_CONVENTION;
 import static org.bson.codecs.pojo.Conventions.USE_GETTERS_FOR_SETTERS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.swing.launcher.ApplicationLauncher.application;
+import static org.assertj.swing.timing.Pause.pause;
+import static org.assertj.swing.timing.Timeout.timeout;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -23,11 +25,16 @@ import javax.swing.JFrame;
 
 import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.finder.WindowFinder;
 import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JButtonFixture;
+import org.assertj.swing.fixture.JLabelFixture;
 import org.assertj.swing.fixture.JListFixture;
+import org.assertj.swing.fixture.JTextComponentFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.assertj.swing.timing.Condition;
 import org.bson.codecs.configuration.CodecProvider;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
@@ -53,9 +60,16 @@ import io.github.marcopaglio.booking.model.Reservation;
 @DisplayName("End-to-end tests for BookingSwingApp usign MongoDB")
 @RunWith(GUITestRunner.class)
 public class MongoBookingSwingAppE2E extends AssertJSwingJUnitTestCase {
+	private static final int TIMEOUT = 5000;
+	private static final int DEFAULT_NUM_OF_CLIENTS = 1;
+
 	private static final String A_FIRSTNAME = "Mario";
 	private static final String A_LASTNAME = "Rossi";
 	private static final UUID A_CLIENT_UUID = UUID.fromString("b8a88ad6-739e-4df5-b3ea-82832a56843a");
+	private static final String ANOTHER_FIRSTNAME = "Maria";
+	private static final String ANOTHER_LASTNAME = "De Lucia";
+	private static final String INVALID_FIRSTNAME = "Mari@";
+	private static final String INVALID_LASTNAME = "De_Lucia";
 
 	private static final String A_YEAR = "2022";
 	private static final String A_MONTH = "04";
@@ -74,6 +88,11 @@ public class MongoBookingSwingAppE2E extends AssertJSwingJUnitTestCase {
 
 	private FrameFixture window;
 
+	private JTextComponentFixture nameFormTxt;
+	private JTextComponentFixture surnameFormTxt;
+	private JLabelFixture formErrorMsgLbl;
+	private JLabelFixture operationErrorMsgLbl;
+	private JButtonFixture addClientBtn;
 	private JListFixture clientList;
 	private JListFixture reservationList;
 
@@ -136,6 +155,17 @@ public class MongoBookingSwingAppE2E extends AssertJSwingJUnitTestCase {
 			}
 		}).using(robot());
 		
+		// text fields
+		nameFormTxt = window.textBox("nameFormTxt");
+		surnameFormTxt = window.textBox("surnameFormTxt");
+		
+		// error labels
+		formErrorMsgLbl = window.label("formErrorMsgLbl");
+		operationErrorMsgLbl = window.label("operationErrorMsgLbl");
+		
+		// buttons
+		addClientBtn = window.button(JButtonMatcher.withText("Add Client"));
+		
 		// lists
 		clientList = window.list("clientList");
 		reservationList = window.list("reservationList");
@@ -146,14 +176,102 @@ public class MongoBookingSwingAppE2E extends AssertJSwingJUnitTestCase {
 		mongoClient.close();
 	}
 
+
 	@Test @GUITest
-	public void testOnStartAllDatabaseElementsAreShown() {
+	@DisplayName("Application is started")
+	public void testApplicationWhenIsStartedShouldShowAllDatabaseElements() {
 		assertThat(clientList.contents())
 			.anySatisfy(e -> assertThat(e).contains(A_FIRSTNAME, A_LASTNAME));
 		
 		assertThat(reservationList.contents())
 			.anySatisfy(e -> assertThat(e).contains(A_LOCALDATE.toString()));
 	}
+
+
+	////////////// Add client
+	@Test @GUITest
+	@DisplayName("Add client is successful")
+	public void testAddClientWhenIsSuccessfulShouldShowTheNewClient() {
+		nameFormTxt.enterText(ANOTHER_FIRSTNAME);
+		surnameFormTxt.enterText(ANOTHER_LASTNAME);
+		
+		addClientBtn.click();
+		
+		pause(
+			new Condition("Client list to contain different number of clients than the default ones") {
+				@Override
+				public boolean test() {
+					return clientList.contents().length != DEFAULT_NUM_OF_CLIENTS;
+				}
+			}
+		, timeout(TIMEOUT));
+		
+		assertThat(clientList.contents())
+			.anySatisfy(e -> assertThat(e).contains(ANOTHER_FIRSTNAME, ANOTHER_LASTNAME));
+	}
+
+	@Test @GUITest
+	@DisplayName("Name is invalid")
+	public void testAddClientWhenNameIsInvalidShouldShowFormError() {
+		nameFormTxt.enterText(INVALID_FIRSTNAME);
+		surnameFormTxt.enterText(ANOTHER_LASTNAME);
+		
+		addClientBtn.click();
+		
+		pause(
+			new Condition("Form error to contain a message") {
+				@Override
+				public boolean test() {
+					return !formErrorMsgLbl.text().isBlank();
+				}
+			}
+		, timeout(TIMEOUT));
+		
+		assertThat(formErrorMsgLbl.text()).contains("name");
+	}
+
+	@Test @GUITest
+	@DisplayName("Surname is invalid")
+	public void testAddClientWhenSurnameIsInvalidShouldShowFormError() {
+		nameFormTxt.enterText(ANOTHER_FIRSTNAME);
+		surnameFormTxt.enterText(INVALID_LASTNAME);
+		
+		addClientBtn.click();
+		
+		pause(
+			new Condition("Form error to contain a message") {
+				@Override
+				public boolean test() {
+					return !formErrorMsgLbl.text().isBlank();
+				}
+			}
+		, timeout(TIMEOUT));
+		
+		assertThat(formErrorMsgLbl.text()).contains("surname");
+	}
+
+	@Test @GUITest
+	@DisplayName("Adding client fails")
+	public void testAddClientWhenIsAFailureShouldShowAnOperationError() {
+		nameFormTxt.enterText(A_FIRSTNAME);
+		surnameFormTxt.enterText(A_LASTNAME);
+		
+		addClientBtn.click();
+		
+		pause(
+			new Condition("Operation error to contain a message") {
+				@Override
+				public boolean test() {
+					return !operationErrorMsgLbl.text().isBlank();
+				}
+			}
+		, timeout(TIMEOUT));
+		
+		assertThat(operationErrorMsgLbl.text()).contains(A_FIRSTNAME, A_LASTNAME);
+	}
+	////////////// Add client
+
+
 
 	public void addTestClientToDatabase(Client client, UUID id) {
 		client.setId(id);
--- booking-app/src/e2e/java/io/github/marcopaglio/booking/app/swing/PostgresBookingSwingAppE2E.java
+++ booking-app/src/e2e/java/io/github/marcopaglio/booking/app/swing/PostgresBookingSwingAppE2E.java
@@ -9,6 +9,8 @@ import static io.github.marcopaglio.booking.model.Client.CLIENT_TABLE_DB;
 import static io.github.marcopaglio.booking.model.Reservation.RESERVATION_TABLE_DB;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.swing.launcher.ApplicationLauncher.application;
+import static org.assertj.swing.timing.Pause.pause;
+import static org.assertj.swing.timing.Timeout.timeout;
 
 import java.time.LocalDate;
 import java.util.Map;
@@ -18,11 +20,16 @@ import javax.swing.JFrame;
 
 import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.finder.WindowFinder;
 import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JButtonFixture;
+import org.assertj.swing.fixture.JLabelFixture;
 import org.assertj.swing.fixture.JListFixture;
+import org.assertj.swing.fixture.JTextComponentFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.assertj.swing.timing.Condition;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -38,9 +45,16 @@ import jakarta.persistence.Persistence;
 @DisplayName("End-to-end tests for BookingSwingApp using PostgreSQL")
 @RunWith(GUITestRunner.class)
 public class PostgresBookingSwingAppE2E extends AssertJSwingJUnitTestCase {
+	private static final int TIMEOUT = 5000;
+	private static final int DEFAULT_NUM_OF_CLIENTS = 1;
+
 	private static final String A_FIRSTNAME = "Mario";
 	private static final String A_LASTNAME = "Rossi";
 	private static final UUID A_CLIENT_UUID = UUID.fromString("500775ab-0ef3-4893-999f-f70098670722");
+	private static final String ANOTHER_FIRSTNAME = "Maria";
+	private static final String ANOTHER_LASTNAME = "De Lucia";
+	private static final String INVALID_FIRSTNAME = "Mari@";
+	private static final String INVALID_LASTNAME = "De_Lucia";
 
 	private static final String A_YEAR = "2022";
 	private static final String A_MONTH = "04";
@@ -49,6 +63,7 @@ public class PostgresBookingSwingAppE2E extends AssertJSwingJUnitTestCase {
 	private static final UUID A_RESERVATION_UUID = UUID.fromString("bfe395cd-08f9-46bd-8080-269d62f0d366");
 
 	private static final String DBMS = "POSTGRES";
+
 	private static String postgresHost = System.getProperty("postgres.host", "localhost");
 	private static int postgresPort = Integer.parseInt(System.getProperty("postgres.port", "5432"));
 	private static String postgresName = System.getProperty("postgres.name", "ITandE2ETest_db");
@@ -59,6 +74,11 @@ public class PostgresBookingSwingAppE2E extends AssertJSwingJUnitTestCase {
 
 	private FrameFixture window;
 
+	private JTextComponentFixture nameFormTxt;
+	private JTextComponentFixture surnameFormTxt;
+	private JLabelFixture formErrorMsgLbl;
+	private JLabelFixture operationErrorMsgLbl;
+	private JButtonFixture addClientBtn;
 	private JListFixture clientList;
 	private JListFixture reservationList;
 
@@ -109,6 +129,17 @@ public class PostgresBookingSwingAppE2E extends AssertJSwingJUnitTestCase {
 			}
 		}).using(robot());
 		
+		// text fields
+		nameFormTxt = window.textBox("nameFormTxt");
+		surnameFormTxt = window.textBox("surnameFormTxt");
+		
+		// error labels
+		formErrorMsgLbl = window.label("formErrorMsgLbl");
+		operationErrorMsgLbl = window.label("operationErrorMsgLbl");
+		
+		// buttons
+		addClientBtn = window.button(JButtonMatcher.withText("Add Client"));
+		
 		// lists
 		clientList = window.list("clientList");
 		reservationList = window.list("reservationList");
@@ -119,14 +150,102 @@ public class PostgresBookingSwingAppE2E extends AssertJSwingJUnitTestCase {
 		emf.close();
 	}
 
+
 	@Test @GUITest
-	public void testOnStartAllDatabaseElementsAreShown() {
+	@DisplayName("Application is started")
+	public void testApplicationWhenIsStartedShouldShowAllDatabaseElements() {
 		assertThat(clientList.contents())
 			.anySatisfy(e -> assertThat(e).contains(A_FIRSTNAME, A_LASTNAME));
 		
 		assertThat(reservationList.contents())
 			.anySatisfy(e -> assertThat(e).contains(A_LOCALDATE.toString()));
 	}
+
+
+	////////////// Add client
+	@Test @GUITest
+	@DisplayName("Adding client succeeds")
+	public void testAddClientWhenIsSuccessfulShouldShowTheNewClient() {
+		nameFormTxt.enterText(ANOTHER_FIRSTNAME);
+		surnameFormTxt.enterText(ANOTHER_LASTNAME);
+		
+		addClientBtn.click();
+		
+		pause(
+			new Condition("Client list to contain different number of clients than the default ones") {
+				@Override
+				public boolean test() {
+					return clientList.contents().length != DEFAULT_NUM_OF_CLIENTS;
+				}
+			}
+		, timeout(TIMEOUT));
+		
+		assertThat(clientList.contents())
+			.anySatisfy(e -> assertThat(e).contains(ANOTHER_FIRSTNAME, ANOTHER_LASTNAME));
+	}
+
+	@Test @GUITest
+	@DisplayName("Name is invalid")
+	public void testAddClientWhenNameIsInvalidShouldShowFormError() {
+		nameFormTxt.enterText(INVALID_FIRSTNAME);
+		surnameFormTxt.enterText(ANOTHER_LASTNAME);
+		
+		addClientBtn.click();
+		
+		pause(
+			new Condition("Form error to contain a message") {
+				@Override
+				public boolean test() {
+					return !formErrorMsgLbl.text().isBlank();
+				}
+			}
+		, timeout(TIMEOUT));
+		
+		assertThat(formErrorMsgLbl.text()).contains("name");
+	}
+
+	@Test @GUITest
+	@DisplayName("Surname is invalid")
+	public void testAddClientWhenSurnameIsInvalidShouldShowFormError() {
+		nameFormTxt.enterText(ANOTHER_FIRSTNAME);
+		surnameFormTxt.enterText(INVALID_LASTNAME);
+		
+		addClientBtn.click();
+		
+		pause(
+			new Condition("Form error to contain a message") {
+				@Override
+				public boolean test() {
+					return !formErrorMsgLbl.text().isBlank();
+				}
+			}
+		, timeout(TIMEOUT));
+		
+		assertThat(formErrorMsgLbl.text()).contains("surname");
+	}
+
+	@Test @GUITest
+	@DisplayName("Adding client fails")
+	public void testAddClientWhenIsAFailureShouldShowAnOperationError() {
+		nameFormTxt.enterText(A_FIRSTNAME);
+		surnameFormTxt.enterText(A_LASTNAME);
+		
+		addClientBtn.click();
+		
+		pause(
+			new Condition("Operation error to contain a message") {
+				@Override
+				public boolean test() {
+					return !operationErrorMsgLbl.text().isBlank();
+				}
+			}
+		, timeout(TIMEOUT));
+		
+		assertThat(operationErrorMsgLbl.text()).contains(A_FIRSTNAME, A_LASTNAME);
+	}
+	////////////// Add client
+
+
 
 	private void addTestClientToDatabase(Client client, UUID id) {
 		EntityManager em = emf.createEntityManager();
