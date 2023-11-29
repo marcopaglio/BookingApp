82ae428746cd64eb5c64096b72eec70dae5ac599
marcopaglio <marco.paglio1997@gmail.com>
2023-11-29 21:44:54 +0100

    Duplicated e2e test in booking-ui-module

---
+++ booking-ui-module/src/e2e/java/io/github/marcopaglio/booking/view/swing/MongoBookingSwingViewE2E.java
@@ -0,0 +1,244 @@
+package io.github.marcopaglio.booking.view.swing;
+
+import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
+import static io.github.marcopaglio.booking.model.Client.CLIENT_TABLE_DB;
+import static io.github.marcopaglio.booking.model.Client.FIRSTNAME_DB;
+import static io.github.marcopaglio.booking.model.Client.LASTNAME_DB;
+import static io.github.marcopaglio.booking.model.Reservation.DATE_DB;
+import static io.github.marcopaglio.booking.model.Reservation.RESERVATION_TABLE_DB;
+import static org.bson.UuidRepresentation.STANDARD;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import static org.bson.codecs.pojo.Conventions.ANNOTATION_CONVENTION;
+import static org.bson.codecs.pojo.Conventions.USE_GETTERS_FOR_SETTERS;
+import static org.assertj.swing.launcher.ApplicationLauncher.application;
+
+import java.awt.EventQueue;
+import java.awt.Frame;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.UUID;
+
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.bson.codecs.configuration.CodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.IndexOptions;
+import com.mongodb.client.model.Indexes;
+
+import io.github.marcopaglio.booking.model.Client;
+import io.github.marcopaglio.booking.model.Reservation;
+import io.github.marcopaglio.booking.presenter.BookingPresenter;
+import io.github.marcopaglio.booking.presenter.served.ServedBookingPresenter;
+import io.github.marcopaglio.booking.repository.factory.ClientRepositoryFactory;
+import io.github.marcopaglio.booking.repository.factory.ReservationRepositoryFactory;
+import io.github.marcopaglio.booking.service.BookingService;
+import io.github.marcopaglio.booking.service.transactional.TransactionalBookingService;
+import io.github.marcopaglio.booking.transaction.handler.factory.TransactionHandlerFactory;
+import io.github.marcopaglio.booking.transaction.manager.TransactionManager;
+import io.github.marcopaglio.booking.transaction.manager.mongo.TransactionMongoManager;
+import io.github.marcopaglio.booking.validator.ClientValidator;
+import io.github.marcopaglio.booking.validator.ReservationValidator;
+import io.github.marcopaglio.booking.validator.restricted.RestrictedClientValidator;
+import io.github.marcopaglio.booking.validator.restricted.RestrictedReservationValidator;
+
+@DisplayName("End-to-end tests for BookingSwingApp usign MongoDB")
+@RunWith(GUITestRunner.class)
+public class MongoBookingSwingViewE2E extends BookingSwingViewE2E {
+	private static final String MONGODB_NAME = "ITandE2ETest_db";
+	private static String mongoHost = System.getProperty("mongo.host", "localhost");
+	private static int mongoPort = Integer.parseInt(System.getProperty("mongo.port", "27017"));
+
+	private static MongoClient mongoClient;
+	private static MongoDatabase database;
+	private static MongoCollection<Client> clientCollection;
+	private static MongoCollection<Reservation> reservationCollection;
+
+	@BeforeClass
+	public static void setupClient() throws Exception {
+		mongoClient = getClient(String.format("mongodb://%s:%d", mongoHost, mongoPort));
+		database = mongoClient.getDatabase(MONGODB_NAME);
+		clientCollection = database.getCollection(CLIENT_TABLE_DB, Client.class);
+		reservationCollection = database.getCollection(RESERVATION_TABLE_DB, Reservation.class);
+	}
+
+	private static MongoClient getClient(String connectionString) {
+		// define the CodecProvider for POJO classes
+		CodecProvider pojoCodecProvider = PojoCodecProvider.builder()
+				.conventions(Arrays.asList(ANNOTATION_CONVENTION, USE_GETTERS_FOR_SETTERS))
+				.automatic(true)
+				.build();
+		
+		// define the CodecRegistry as codecs and other related information
+		CodecRegistry pojoCodecRegistry =
+				fromRegistries(getDefaultCodecRegistry(),
+				fromProviders(pojoCodecProvider));
+		
+		// configure the MongoClient for using the CodecRegistry
+		MongoClientSettings settings = MongoClientSettings.builder()
+				.applyConnectionString(new ConnectionString(connectionString))
+				.uuidRepresentation(STANDARD)
+				.codecRegistry(pojoCodecRegistry)
+				.build();
+		return MongoClients.create(settings);
+	}
+
+	@Override
+	protected void onSetUp() throws Exception {
+		// make sure we always start with a clean database
+		database.drop();
+		
+		// add entities to the database
+		addTestClientToDatabase(A_FIRSTNAME, A_LASTNAME, A_CLIENT_UUID);
+		addTestReservationToDatabase(A_CLIENT_UUID, A_DATE, A_RESERVATION_UUID);
+		
+		// start the Swing application
+		application(MongoBookingSwingApp.class)
+			.withArgs(
+					mongoHost,
+					String.valueOf(mongoPort),
+					MONGODB_NAME
+			).start();
+		
+		super.onSetUp();
+	}
+
+	@AfterClass
+	public static void closeClient() throws Exception {
+		mongoClient.close();
+	}
+
+	@Override
+	protected void addTestClientToDatabase(String name, String surname, UUID id) {
+		Client client = new Client(name, surname);
+		client.setId(id);
+		ClientSession session = mongoClient.startSession();
+		clientCollection.createIndex(session,
+				Indexes.descending(FIRSTNAME_DB, LASTNAME_DB), new IndexOptions().unique(true));
+		clientCollection.insertOne(session, client);
+		session.close();
+	}
+
+	@Override
+	protected void removeTestClientFromDatabase(String name, String surname) {
+		ClientSession session = mongoClient.startSession();
+		clientCollection.deleteOne(session, Filters.and(
+						Filters.eq(FIRSTNAME_DB, name),
+						Filters.eq(LASTNAME_DB, surname)));
+		session.close();
+	}
+
+	@Override
+	protected void addTestReservationToDatabase(UUID clientId, String date, UUID id) {
+		Reservation reservation = new Reservation(clientId,  LocalDate.parse(date));
+		reservation.setId(id);
+		ClientSession session = mongoClient.startSession();
+		reservationCollection.createIndex(session,
+				Indexes.descending(DATE_DB), new IndexOptions().unique(true));
+		reservationCollection.insertOne(session, reservation);
+		session.close();
+	}
+
+	@Override
+	protected void removeTestReservationFromDatabase(String date) {
+		ClientSession session = mongoClient.startSession();
+		reservationCollection.deleteOne(session, Filters.eq(DATE_DB, LocalDate.parse(date)));
+		session.close();
+	}
+
+	static class MongoBookingSwingApp {
+		private static final int STARTUP_FAILURE_STATUS = 255;
+
+		private static MongoClient mongoAppClient;
+
+		public static void main(String[] args) {
+			EventQueue.invokeLater(() -> {
+				try {
+					String mongoHost = "localhost";
+					int mongoPort = 27017;
+					String mongoName = "BookingApp_MongoDB";
+					if (args.length > 0)
+						mongoHost = args[0];
+					if (args.length > 1)
+						mongoPort = Integer.parseInt(args[1]);
+					if (args.length > 2)
+						mongoName = args[2];
+					
+					TransactionHandlerFactory transactionHandlerFactory = new TransactionHandlerFactory();
+					ClientRepositoryFactory clientRepositoryFactory = new ClientRepositoryFactory();
+					ReservationRepositoryFactory reservationRepositoryFactory = new ReservationRepositoryFactory();
+					
+					mongoAppClient = getClient(String.format("mongodb://%s:%d", mongoHost, mongoPort));
+					TransactionManager transactionManager = new TransactionMongoManager(mongoAppClient, mongoName,
+							transactionHandlerFactory, clientRepositoryFactory, reservationRepositoryFactory);
+					
+					BookingService bookingService = new TransactionalBookingService(transactionManager);
+					ClientValidator clientValidator = new RestrictedClientValidator();
+					ReservationValidator reservationValidator = new RestrictedReservationValidator();
+					
+					BookingSwingView bookingSwingView = new BookingSwingView();
+					BookingPresenter bookingPresenter = new ServedBookingPresenter(bookingSwingView,
+							bookingService, clientValidator, reservationValidator);
+					bookingSwingView.setBookingPresenter(bookingPresenter);
+					bookingSwingView.setVisible(true);
+					bookingPresenter.allClients();
+					bookingPresenter.allReservations();
+				} catch(Exception e) {
+					closeWindows();
+					System.exit(STARTUP_FAILURE_STATUS);
+				}
+			});
+			
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				@Override
+				public void run() {
+					if (mongoAppClient != null)
+						mongoAppClient.close();
+				}
+			});
+		}
+
+		private static MongoClient getClient(String connectionString) {
+			// define the CodecProvider for POJO classes
+			CodecProvider pojoCodecProvider = PojoCodecProvider.builder()
+					.conventions(Arrays.asList(ANNOTATION_CONVENTION, USE_GETTERS_FOR_SETTERS))
+					.automatic(true)
+					.build();
+			
+			// define the CodecRegistry as codecs and other related information
+			CodecRegistry pojoCodecRegistry =
+					fromRegistries(getDefaultCodecRegistry(),
+					fromProviders(pojoCodecProvider));
+			
+			// configure the MongoClient for using the CodecRegistry
+			MongoClientSettings settings = MongoClientSettings.builder()
+					.applyConnectionString(new ConnectionString(connectionString))
+					.uuidRepresentation(STANDARD)
+					.codecRegistry(pojoCodecRegistry)
+					.build();
+			return MongoClients.create(settings);
+		}
+
+		private static void closeWindows() {
+			for (Frame f: Frame.getFrames()) {
+				if (f.isDisplayable()) {
+					f.setVisible(false);
+					f.dispose();
+				}
+			}
+		}
+	}
+}
---
+++ booking-ui-module/src/e2e/java/io/github/marcopaglio/booking/view/swing/PostgresBookingSwingViewE2E.java
@@ -0,0 +1,212 @@
+package io.github.marcopaglio.booking.view.swing;
+
+import static io.github.marcopaglio.booking.model.BaseEntity.ID_POSTGRESQL;
+import static io.github.marcopaglio.booking.model.Client.FIRSTNAME_DB;
+import static io.github.marcopaglio.booking.model.Client.LASTNAME_DB;
+import static io.github.marcopaglio.booking.model.Reservation.CLIENTID_DB;
+import static io.github.marcopaglio.booking.model.Reservation.DATE_DB;
+import static io.github.marcopaglio.booking.model.Client.CLIENT_TABLE_DB;
+import static io.github.marcopaglio.booking.model.Reservation.RESERVATION_TABLE_DB;
+import static org.assertj.swing.launcher.ApplicationLauncher.application;
+
+import java.awt.EventQueue;
+import java.awt.Frame;
+import java.util.Map;
+import java.util.UUID;
+
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+
+import io.github.marcopaglio.booking.presenter.BookingPresenter;
+import io.github.marcopaglio.booking.presenter.served.ServedBookingPresenter;
+import io.github.marcopaglio.booking.repository.factory.ClientRepositoryFactory;
+import io.github.marcopaglio.booking.repository.factory.ReservationRepositoryFactory;
+import io.github.marcopaglio.booking.service.BookingService;
+import io.github.marcopaglio.booking.service.transactional.TransactionalBookingService;
+import io.github.marcopaglio.booking.transaction.handler.factory.TransactionHandlerFactory;
+import io.github.marcopaglio.booking.transaction.manager.TransactionManager;
+import io.github.marcopaglio.booking.transaction.manager.postgres.TransactionPostgresManager;
+import io.github.marcopaglio.booking.validator.ClientValidator;
+import io.github.marcopaglio.booking.validator.ReservationValidator;
+import io.github.marcopaglio.booking.validator.restricted.RestrictedClientValidator;
+import io.github.marcopaglio.booking.validator.restricted.RestrictedReservationValidator;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+@DisplayName("End-to-end tests for BookingSwingApp using PostgreSQL")
+@RunWith(GUITestRunner.class)
+public class PostgresBookingSwingViewE2E extends BookingSwingViewE2E {
+	private static String postgresHost = System.getProperty("postgres.host", "localhost");
+	private static int postgresPort = Integer.parseInt(System.getProperty("postgres.port", "5432"));
+	private static String postgresName = System.getProperty("postgres.name", "ITandE2ETest_db");
+	private static String postgresUser = System.getProperty("postgres.user", "postgres-it-e2e");
+	private static String postgresPswd = System.getProperty("postgres.pswd", "postgres-it-e2e");
+
+	private static EntityManagerFactory emf;
+
+	@BeforeClass
+	public static void setupEmf() throws Exception {
+		// ATTENTION: number of max connection for PostgreSQL database is settled in
+		// docker-maven-plugin (pom.xml) and is strictly related to the number of current tests.
+		// Increasing the number of e2e tests requires more connections.
+		String jdbcUrl = String.format("jdbc:postgresql://%s:%d/%s",
+				postgresHost, postgresPort, postgresName);
+		emf = Persistence.createEntityManagerFactory("postgres-e2e", Map.of(
+				"jakarta.persistence.jdbc.url", jdbcUrl,
+				"jakarta.persistence.jdbc.user", postgresUser,
+				"jakarta.persistence.jdbc.password", postgresPswd));
+	}
+
+	@Override
+	protected void onSetUp() throws Exception {
+		// make sure we always start with a clean database
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.createNativeQuery("TRUNCATE TABLE " + CLIENT_TABLE_DB + "," + RESERVATION_TABLE_DB).executeUpdate();
+		em.getTransaction().commit();
+		em.close();
+		
+		// add entities to the database
+		addTestClientToDatabase(A_FIRSTNAME, A_LASTNAME, A_CLIENT_UUID);
+		addTestReservationToDatabase(A_CLIENT_UUID, A_DATE, A_RESERVATION_UUID);
+		
+		// start the Swing application
+		application(PostgresBookingSwingApp.class)
+			.withArgs(
+					postgresHost,
+					String.valueOf(postgresPort),
+					postgresName,
+					postgresUser,
+					postgresPswd
+			).start();
+		
+		super.onSetUp();
+	}
+
+	@AfterClass
+	public static void closeEmf() throws Exception {
+		emf.close();
+	}
+
+	@Override
+	protected void addTestClientToDatabase(String name, String surname, UUID id) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.createNativeQuery("INSERT INTO " + CLIENT_TABLE_DB + " " +
+							"(" + ID_POSTGRESQL + ", "+ FIRSTNAME_DB + ", " + LASTNAME_DB + ") " +
+							"VALUES ('" + id + "', '" + name + "', '" + surname + "') ")
+			.executeUpdate();
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	@Override
+	protected void removeTestClientFromDatabase(String name, String surname) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.createNativeQuery("DELETE FROM " + CLIENT_TABLE_DB + " " +
+							"WHERE " + FIRSTNAME_DB + "='" + name +"' " +
+							"AND " + LASTNAME_DB + "='" + surname +"' ")
+			.executeUpdate();
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	@Override
+	protected void addTestReservationToDatabase(UUID clientId, String date, UUID id) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.createNativeQuery("INSERT INTO " + RESERVATION_TABLE_DB + " " +
+							"(" + ID_POSTGRESQL + ", "+ CLIENTID_DB + ", " + DATE_DB + ") " +
+							"VALUES ('" + id + "', '" + clientId + "', '" + date + "') ")
+			.executeUpdate();
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	@Override
+	protected void removeTestReservationFromDatabase(String date) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.createNativeQuery("DELETE FROM " + RESERVATION_TABLE_DB + " " +
+							"WHERE " + DATE_DB + "='" + date +"' ")
+			.executeUpdate();
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	static class PostgresBookingSwingApp {
+		private static final int STARTUP_FAILURE_STATUS = 255;
+
+		private static EntityManagerFactory entityManagerAppFactory;
+
+		public static void main(String[] args) {
+			EventQueue.invokeLater(() -> {
+				try {
+					String postgresHost = "localhost";
+					int postgresPort = 5432;
+					String postgresName = "BookingApp_PostgreSQL";
+					String postgresUser = "postgres-user";
+					String postgresPswd = "postgres-pswd";
+					if (args.length > 0)
+						postgresHost = args[0];
+					if (args.length > 1)
+						postgresPort = Integer.parseInt(args[1]);
+					if (args.length > 2)
+						postgresName = args[2];
+					if (args.length > 3)
+						postgresUser = args[3];
+					if (args.length > 4)
+						postgresPswd = args[4];
+					
+					TransactionHandlerFactory transactionHandlerFactory = new TransactionHandlerFactory();
+					ClientRepositoryFactory clientRepositoryFactory = new ClientRepositoryFactory();
+					ReservationRepositoryFactory reservationRepositoryFactory = new ReservationRepositoryFactory();
+					
+					entityManagerAppFactory = Persistence.createEntityManagerFactory("postgres-e2e", Map.of(
+							"jakarta.persistence.jdbc.url", String.format("jdbc:postgresql://%s:%d/%s", postgresHost, postgresPort, postgresName),
+							"jakarta.persistence.jdbc.user", postgresUser,
+							"jakarta.persistence.jdbc.password", postgresPswd));
+					TransactionManager transactionManager = new TransactionPostgresManager(entityManagerAppFactory,
+							transactionHandlerFactory, clientRepositoryFactory, reservationRepositoryFactory);
+					
+					BookingService bookingService = new TransactionalBookingService(transactionManager);
+					ClientValidator clientValidator = new RestrictedClientValidator();
+					ReservationValidator reservationValidator = new RestrictedReservationValidator();
+					
+					BookingSwingView bookingSwingView = new BookingSwingView();
+					BookingPresenter bookingPresenter = new ServedBookingPresenter(bookingSwingView,
+							bookingService, clientValidator, reservationValidator);
+					bookingSwingView.setBookingPresenter(bookingPresenter);
+					bookingSwingView.setVisible(true);
+					bookingPresenter.allClients();
+					bookingPresenter.allReservations();
+				} catch(Exception e) {
+					closeWindows();
+					System.exit(STARTUP_FAILURE_STATUS);
+				}
+			});
+			
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				@Override
+				public void run() {
+					if (entityManagerAppFactory != null && entityManagerAppFactory.isOpen())
+						entityManagerAppFactory.close();
+				}
+			});
+		}
+
+		private static void closeWindows() {
+			for (Frame f: Frame.getFrames()) {
+				if (f.isDisplayable()) {
+					f.setVisible(false);
+					f.dispose();
+				}
+			}
+		}
+	}
+}
